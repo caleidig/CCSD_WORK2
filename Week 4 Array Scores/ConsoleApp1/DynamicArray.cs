6085426804de9cc81ca3e5058f3f323e5329ec77
Chris Leidig <caleidig@gmail.com>
2018-03-28 12:21:20 -0700

    Week 4 Arrays
    week 4 array of scores

---
+++ Week 4 Array Scores/ConsoleApp1/DynamicArray.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DynamicArray
+    {
+        private double[] m_array;
+        private int m_numValsAdded;
+        double minVal;
+        int minValLoc;
+        double maxVal;
+        int maxValLoc;
+
+        public DynamicArray()
+        {
+            m_array = new double[1];
+            m_numValsAdded = 0;
+        }
+        public int Length
+        {
+            get
+            {
+                return m_array.Length;
+            }
+        }
+
+        public void AddToArray(double val)
+        {
+            m_numValsAdded++;
+            if (m_numValsAdded <= m_array.Length)
+            {
+                m_array[m_numValsAdded - 1] = val;
+            }
+            else
+            {
+                double[] temp_array = new double[m_numValsAdded + 1];
+
+                for (int i = 0; i < m_array.Length; i++)
+                {
+                    temp_array[i] = m_array[i];
+                }
+
+                System.Array.Resize(ref m_array, (m_numValsAdded + 1));
+
+                for (int i = 0; i < m_array.Length; i++)
+                {
+                    m_array[i] = temp_array[i];
+                }
+                
+                m_array[m_numValsAdded] = val;
+            }
+        }
+
+        public double this[int index]
+        {
+            get
+            { 
+                 return m_array [index];
+             }
+
+            set
+            {
+                m_array[index] = value;
+            }
+        }
+
+        public double FindMinVal()
+        {
+
+                minVal = m_array[0];
+                for (int i = 0; i < m_numValsAdded; i++)
+                {
+                    if (m_array[i] < minVal)
+                    {
+                        minVal = m_array[i];
+                        minValLoc = i;
+                    }
+                }
+                return minVal;
+
+        }
+        public double FindMaxVal()
+        {
+
+                maxVal = m_array[0];
+                for (int i = 0; i < m_numValsAdded; i++)
+                {
+                    if (m_array[i] > maxVal)
+                    {
+                        maxVal = m_array[i];
+                        maxValLoc = i;
+                    }
+                }
+                return maxVal;
+
+        }
+        public void RemoveValue()
+        { }
+    }
+}
---
+++ Week 4 Array Scores/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Scores Below. Any input outside of 1-10 will show results.");
+            Console.WriteLine();
+            DynamicArray studentArray = new DynamicArray();
+            double input=25;
+            int counter = 0;
+            while (input != 0)
+            { 
+
+                Console.Write("Enter score #" + (counter+1) + ": ");
+
+                input = Convert.ToDouble(Console.ReadLine());
+                
+                if (input < 1 || input > 10)
+                { break; }
+                //if (studentArray.Length <= counter+1)
+                    studentArray.AddToArray(input);
+                //else
+                //{
+                    studentArray[counter] = input;
+                //}
+                counter++;
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < studentArray.Length-1;i++)
+            {
+                Console.WriteLine("Score " + (i+1) + ": " + studentArray[i]);
+            }
+
+            Console.WriteLine("Lowest Score: " + studentArray.FindMinVal());
+            Console.WriteLine("Highest Score: " + studentArray.FindMaxVal());
+            Console.ReadKey();
+
+            double total = 0;
+            for (int i = 0; i <= studentArray.Length-1;i++)
+            {
+                total += studentArray[i];
+            }
+            total -= studentArray.FindMaxVal();
+            total -= studentArray.FindMinVal();
+            Console.WriteLine("total: " + total);
+            Console.WriteLine("# scores: " + (studentArray.Length-3));
+            Console.WriteLine("The Average without the Highest and Lowest grade is: " + (total / (studentArray.Length - 3)));
+            Console.ReadKey();
+        }
+    }
+}
